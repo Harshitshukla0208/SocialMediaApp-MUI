72eef32064b6cacb3df528b1d8e6be5f3c378ea8
Harshit Shukla <mrharshit2409@gmail.com>
2024-02-17 01:59:25 +0530

    added a post creation function

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,10 +2,29 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
+import axios from 'axios';
 
 function Input(){
     const [postText, setPostText] = useState("");
     const [imgUrl, setImgUrl] = useState("");
+
+    async function createPost(){
+        console.log("inside function")
+        axios.post("https://dummyapi.io/data/v1/post/create", 
+        {
+            owner: '60d0fe4f5311236168a109cb',
+            text: postText,
+            image: imgUrl,
+            likes: 0,
+            publishDate: new Date()
+        },
+        {
+            headers: {'app-id': import.meta.env.VITE_API_ID}
+        })
+        .then(response => {
+            console.log(response.data)
+        })
+    }
 
     return (
         <Box sx={{mt:'1rem'}}>
@@ -26,7 +45,7 @@ function Input(){
                 onChange={(e) => setImgUrl(e.target.value)}
             />
 
-            <Button 
+            <Button onClick={createPost}
                 sx={{mt:'1rem'}} 
                 variant="contained">Submit</Button>
         </Box>
