410097578181e2738e473a5e1808ceeb7587d65f
Harshit Shukla <mrharshit2409@gmail.com>
2024-02-17 19:21:17 +0530

    cleaner way of doing routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
-import SocialApp from './components/SocialApp'
+import SocialApp from './components/SocialApp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserDetails from './components/UserDetails/UserDetails'
 
 function App() {
-
   return (
-    <>
-      <SocialApp />
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<SocialApp />} />
+        <Route path="/user/:id" element={<UserDetails />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import UserDetails from './components/UserDetails/UserDetails.jsx'
 import './index.css'
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-    },
-    {
-      path: "/user/:id",
-      element: <UserDetails />,
-    },
-  ]
-);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
